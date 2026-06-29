581075ef1a609dd25c1d2674c1f8c34b453d021c
LACLAC-DuyHoang <hoangduydo20@gmail.com>
2026-06-29 22:37:19 +0700

    Add project title to README.md

---
+++ src/DataStructuresAndAlgorithms/Queue.kt
@@ -0,0 +1,6 @@
+package DataStructuresAndAlgorithms
+
+/*
+*   Queue (Hàng đợi): hoạt động theo nguyên tắc FIFO (First In, First Out) -> Vào trước → Ra trước
+*   VD: xếp hàng mua vé thì người đến  trước được phục vụ trước
+* */
